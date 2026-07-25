aebd946e41c6626566f2a0ea92bd971e51f69d49
Sahil Dalvi <sahildalvi4891@gmail.com>
2026-07-25 22:45:33 +0530

    Adding address and payment reducers to Redux store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import cartReducer from './slices/cartSlice';
 import wishlistReducer from './slices/wishlistSlice';
+import addressReducer from './slices/addressSlice';
+import paymentReducer from './slices/paymentSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
     wishlist: wishlistReducer,
+    address: addressReducer,
+    payment: paymentReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
