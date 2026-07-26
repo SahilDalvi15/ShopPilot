674f2270cd6666370a45afa1fe8b2426dfcd204a
Sahil Dalvi <sahildalvi4891@gmail.com>
2026-07-26 20:35:05 +0530

    Integrating ErrorBoundary in main.jsx to wrap the entire app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ToastProvider } from './contexts/ToastContext'
+import ErrorBoundary from './components/ErrorBoundary'
 import './index.css'
 import App from './App.jsx'
 import store from './store/store'
@@ -11,13 +12,15 @@ import ToastContainer from './components/ToastContainer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <ToastProvider>
-          <App />
-          <ToastContainer />
-        </ToastProvider>
-      </QueryClientProvider>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <ToastProvider>
+            <App />
+            <ToastContainer />
+          </ToastProvider>
+        </QueryClientProvider>
+      </Provider>
+    </ErrorBoundary>
   </StrictMode>,
 )
