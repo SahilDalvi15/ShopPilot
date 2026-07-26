5218b70c319cabd7de3419128640d728512f8c1f
Sahil Dalvi <sahildalvi4891@gmail.com>
2026-07-26 18:51:42 +0530

    Integrating ToastProvider and ToastContainer in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ToastProvider } from './contexts/ToastContext'
 import './index.css'
 import App from './App.jsx'
 import store from './store/store'
 import queryClient from './lib/react-query'
+import ToastContainer from './components/ToastContainer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ToastProvider>
+          <App />
+          <ToastContainer />
+        </ToastProvider>
       </QueryClientProvider>
     </Provider>
   </StrictMode>,
