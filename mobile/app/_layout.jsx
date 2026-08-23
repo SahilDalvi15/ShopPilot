55ca8e6e1f00c0f336fb19962be3b09a5235d4b0
Sahil Dalvi <sahildalvi4891@gmail.com>
2026-08-23 12:46:02 +0530

    Hide product details header and setup compare context provider

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,14 +1,19 @@
 import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/AuthContext';
 
+import { CompareProvider } from '../context/CompareContext';
+
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
+      <CompareProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="product/[id]" options={{ headerShown: false }} />
+        </Stack>
+      </CompareProvider>
     </AuthProvider>
   );
 }
